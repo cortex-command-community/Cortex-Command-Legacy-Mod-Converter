9680822d878b1dfc46d074fbefea9ae2fbf75e69
MyNameIsTrez <welfje@gmail.com>
2020-07-20 17:40:53 +0200

    Removed folder zipping code, as it is annoying when you want to manually edit the folder afterwards.

--- convert.py
+++ convert.py
@@ -1,46 +1,50 @@
 import os
-import pathlib
-import shutil
+from pathlib import Path
+from shutil import copyfile
 
 replace_variables_list = {
-	"= Sound"    : "= SoundContainer", # ReloadEndSound *= Sound* -> ReloadEndSound *= SoundContainer*
-	"= AddSample": "= AddSound",
+	"= Sound"    : "= SoundContainer",
+	"AddSample =": "AddSound =",
 }
 
-replace_folders_list = {
-
+manually_replace_variables_list = {
+	"Priority =": "// Priority =",
 }
+
+# replace_folders_list = {
+
+# }
 
 text_file_extensions = {
 	".ini"
 }
 
-def replace_variables(line):
-	for old_str, new_str in replace_variables_list.items():
-		line = line.replace(old_str, new_str)
-	return line
+line_number = 69
 
-for input_folder, input_subfolders, full_filenames in os.walk("input"):
-	# print(input_folder, input_subfolders, full_filenames)
-	output_folder = os.path.join("output", pathlib.Path(*pathlib.Path(input_folder).parts[1:]))
-	if input_folder != "input":
-		os.makedirs(output_folder)
-	for full_filename in full_filenames:
-		filename, file_extension = os.path.splitext(full_filename)
-		if filename == ".empty":
-			continue
-		input_file_path  = os.path.join(input_folder , full_filename)
-		output_file_path = os.path.join(output_folder, full_filename)
-		if file_extension in text_file_extensions:
-			with open(input_file_path, "r") as file_in:
-				text = file_in.read()
-				text = replace_variables(text)
-				with open(output_file_path, "w") as file_out:
-					file_out.write(text)
-		else:
-			shutil.copyfile(input_file_path, output_file_path)
-
-dir_name = "output/mario-b33-manual-conversion.rte"
-shutil.make_archive(dir_name, "zip", dir_name)
-
-shutil.rmtree(dir_name)
+with open("output/manually-edit-these-lines.txt", "w") as file_manual:
+	for input_folder, input_subfolders, full_filenames in os.walk("input"):
+		# print(input_folder, input_subfolders, full_filenames)
+		output_folder = os.path.join("output", Path(*Path(input_folder).parts[1:]))
+		if input_folder != "input":
+			os.makedirs(output_folder)
+		for full_filename in full_filenames:
+			filename, file_extension = os.path.splitext(full_filename)
+			if filename == ".empty":
+				continue
+			input_file_path  = os.path.join(input_folder , full_filename)
+			output_file_path = os.path.join(output_folder, full_filename)
+			if file_extension in text_file_extensions:
+				with open(input_file_path, "r") as file_in:
+					text = file_in.read()
+					# replace variables
+					for old_str, new_str in replace_variables_list.items():
+						text = text.replace(old_str, new_str)
+					# replace variables that need to be manually edited afterwards
+					for old_str, new_str in manually_replace_variables_list.items():
+						text = text.replace(old_str, new_str)
+						# TODO: Add file line numbers to this as well.
+						file_manual.write("file: {} | line: {} | edit this: {}\n".format(output_file_path, line_number, new_str))
+					with open(output_file_path, "w") as file_out:
+						file_out.write(text)
+			else:
+				copyfile(input_file_path, output_file_path)
