d7186d61b17bed5cb6339bf4bbbbfca940ccf8cd
MyNameIsTrez <welfje@gmail.com>
2020-07-20 02:09:32 +0200

    convert.py is now capable of opening of reading the lines in a file, replacing words in it, and writing it back to a new file.

--- convert.py
+++ convert.py
@@ -1,15 +1,30 @@
-replace_variables = {
+import os
+
+replace_variables_list = {
 	"= Sound"    : "= SoundContainer", # ReloadEndSound *= Sound* -> ReloadEndSound *= SoundContainer*
 	"= AddSample": "= AddSound",
 }
 
-replace_folders = {
+replace_folders_list = {
 
 }
 
-# print(replace_variables)
-# print(replace_folders)
+def replace_variables(line):
+	for old_str, new_str in replace_variables_list.items():
+		line = line.replace(old_str, new_str)
+	return line
 
-with open("input\mario-b33-manual-conversion.rte\Mario.rte\Items\Feather\Feather.ini", "r") as fileIn:
-	for line in fileIn:
-		print(line, end="")
+path = "mario-b33-manual-conversion.rte\Mario.rte\Items\Feather\Feather.ini"
+input_path = os.path.join("input", path)
+output_path = os.path.join("output", path)
+
+with open(input_path, "r") as file_in:
+	text = file_in.read()
+	text = replace_variables(text)
+
+	folder_path = os.path.dirname(output_path)
+	print(folder_path)
+	os.makedirs(folder_path)
+
+	with open(output_path, "w") as file_out:
+		file_out.write(text)
