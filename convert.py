e868e85c28a76eb7929ab5563a371d07be813a74
MyNameIsTrez <welfje@gmail.com>
2020-07-20 16:51:08 +0200

    Outputting a .zip now.

--- convert.py
+++ convert.py
@@ -1,4 +1,6 @@
 import os
+import pathlib
+import shutil
 
 replace_variables_list = {
 	"= Sound"    : "= SoundContainer", # ReloadEndSound *= Sound* -> ReloadEndSound *= SoundContainer*
@@ -9,22 +11,36 @@ replace_folders_list = {
 
 }
 
+text_file_extensions = {
+	".ini"
+}
+
 def replace_variables(line):
 	for old_str, new_str in replace_variables_list.items():
 		line = line.replace(old_str, new_str)
 	return line
 
-path = "mario-b33-manual-conversion.rte\Mario.rte\Items\Feather\Feather.ini"
-input_path = os.path.join("input", path)
-output_path = os.path.join("output", path)
+for input_folder, input_subfolders, full_filenames in os.walk("input"):
+	# print(input_folder, input_subfolders, full_filenames)
+	output_folder = os.path.join("output", pathlib.Path(*pathlib.Path(input_folder).parts[1:]))
+	if input_folder != "input":
+		os.makedirs(output_folder)
+	for full_filename in full_filenames:
+		filename, file_extension = os.path.splitext(full_filename)
+		if filename == ".empty":
+			continue
+		input_file_path  = os.path.join(input_folder , full_filename)
+		output_file_path = os.path.join(output_folder, full_filename)
+		if file_extension in text_file_extensions:
+			with open(input_file_path, "r") as file_in:
+				text = file_in.read()
+				text = replace_variables(text)
+				with open(output_file_path, "w") as file_out:
+					file_out.write(text)
+		else:
+			shutil.copyfile(input_file_path, output_file_path)
 
-with open(input_path, "r") as file_in:
-	text = file_in.read()
-	text = replace_variables(text)
+dir_name = "output/mario-b33-manual-conversion.rte"
+shutil.make_archive(dir_name, "zip", dir_name)
 
-	folder_path = os.path.dirname(output_path)
-	print(folder_path)
-	os.makedirs(folder_path)
-
-	with open(output_path, "w") as file_out:
-		file_out.write(text)
+shutil.rmtree(dir_name)
