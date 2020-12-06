2ba4135fe8b29c3b6766a235fe374f0c3ec4f8f2
MyNameIsTrez <welfje@gmail.com>
2020-12-06 01:58:53 +0100

    Prettified and added GitHub and Discord buttons.

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Download the [***Legacy Mod Converter***](http://www.mediafire.com/file/b2qdhe55
 
 If you get a "`Microsoft Defender SmartScreen prevented an unrecognized app from starting. Running this app might put your PC at risk.`" popup message when trying to run the program just press `More info` -> `Run anyway`. It may take a very long time for the program to appear, which is an issue with Windows' Antimalware Service Executable. We're looking into how we can fix both of these issues.
 
-![Legacy mod converter program screenshot](media/cclmc-program-screenshot.png)
+![Legacy mod converter program screenshot](media/cclmc-screenshot.png)
 
 ## Disclaimer
 This program will do most of the conversion work for you, but some conversion steps are too hard to automate, so it's likely that `CCCP` will still crash or print errors in the console. If this happens you'll have to manually fix these issues in the legacy mod folder. If you want to properly fix these errors then you should download this repository and follow the instructions below so you can add your own conversion cases to `conversion_rules.py`.
@@ -21,7 +21,7 @@ This program will do most of the conversion work for you, but some conversion st
 * Run `python gui.py` when you want to convert old mods.
 * `conversion_rules.py` has the `conversion_rules` dictionary inside of it. The dictionary contains entries that look like `'foo': 'bar',` which means means that any time `foo` will be encountered by `convert.py`, it will replace it with `bar`. You can add extra cases to it, see the next sections on how to do this.
 
-## Detailed tutorials on creating conversion rules
+## Tutorials on constructing conversion rules efficiently
 * [Fixing CCCP crashes/errors with the program Everything](https://github.com/cortex-command-community/Cortex-Command-Legacy-Mod-Converter/wiki/Fixing-CCCP-crashes-errors-with-the-program-Everything)
 
 ## Contributing
--- gui.py
+++ gui.py
@@ -1,9 +1,18 @@
 # Run: python gui.py
 # Build: pyinstaller --noconsole --onefile --icon=media/cclmc-icon.ico --name="Legacy Mod Converter" gui.py
 
-import os.path, pathlib
+import os.path, pathlib, webbrowser
 import PySimpleGUI as sg
 import config, convert
+
+
+no_path_set_color = "#b35858"
+path_set_color = "#528b30"
+progress_bar_color = "#17569c"
+
+sg.theme("DarkGrey14")
+sg.theme_input_background_color(path_set_color)
+sg.theme_progress_bar_color((progress_bar_color, sg.theme_progress_bar_color()[1]))
 
 
 def get_folder_containing_mods(mods_folder):
@@ -22,7 +31,7 @@ paths_column = [
 			size=(25, 1),
 			enable_events=True,
 			key="-MODS FOLDER-",
-			background_color="light green" if sg.user_settings_get_entry("mods_folder") else "pink"
+			background_color=sg.theme_input_background_color() if sg.user_settings_get_entry("mods_folder") else no_path_set_color
 		),
 		sg.FolderBrowse()
 	],
@@ -33,11 +42,11 @@ paths_column = [
 			size=(25, 1),
 			enable_events=True,
 			key="-OUTPUT FOLDER-",
-			background_color="light green" if sg.user_settings_get_entry("output_folder") else "pink"
+			background_color=sg.theme_input_background_color() if sg.user_settings_get_entry("output_folder") else no_path_set_color
 		),
 		sg.FolderBrowse()
 	]
-	], title="Paths", element_justification="right")],
+	], title="Paths", element_justification="right")]
 ]
 
 options_column = [
@@ -50,8 +59,15 @@ options_column = [
 run_column = [
 	[sg.Frame(layout=[
 		[sg.Button("Convert", key="-CONVERT-")],
-		[sg.ProgressBar(100, size=(22.5, 20), key="-PROGRESS BAR-")]
-	], title="Run", element_justification="center")],
+		[sg.ProgressBar(100, size=(17.45, 20), key="-PROGRESS BAR-")]
+	], title="Run", element_justification="center")]
+]
+
+info_column = [
+	[sg.Frame(layout=[
+		[sg.Image("media/github-icon.png", enable_events=True, key="-GITHUB-")],
+		[sg.Image("media/discord-icon.png", enable_events=True, key="-DISCORD-")]
+	], title="", pad=(0, (8, 0)))]
 ]
 
 layout = [
@@ -60,12 +76,13 @@ layout = [
 	],
 	[
 		sg.Column(options_column),
-		sg.Column(run_column)
+		sg.Column(run_column),
+		sg.Column(info_column)
 	]
 ]
 
 config.sg = sg
-window = sg.Window("Legacy Mod Converter - v1.0", layout, icon="media/cclmc-icon.ico")
+window = sg.Window("Legacy Mod Converter - v1.0", layout, icon="media/cclmc-icon.ico", button_color=(sg.theme_text_color(), "#2a3948"))
 config.progress_bar = window["-PROGRESS BAR-"]
 
 
@@ -76,16 +93,20 @@ while True:
 		window.close()
 		break
 
-	if event == "-MODS FOLDER-":
+	if event == "-GITHUB-":
+		webbrowser.open("https://github.com/cortex-command-community/Cortex-Command-Legacy-Mod-Converter")
+	elif event == "-DISCORD-":
+		webbrowser.open("https://discord.gg/SdNnKJN")
+	elif event == "-MODS FOLDER-":
 		mods_folder = values[event]
 		if mods_folder != "":
 			sg.user_settings_set_entry("mods_folder", get_folder_containing_mods(mods_folder))
-			window[event](background_color="light green")
+			window[event](background_color=sg.theme_input_background_color())
 	elif event == "-OUTPUT FOLDER-":
 		output_folder = values[event]
 		if output_folder != "":
 			sg.user_settings_set_entry("output_folder", output_folder)
-			window[event](background_color="light green")
+			window[event](background_color=sg.theme_input_background_color())
 	elif event == "-CONVERT-":
 		if sg.user_settings_get_entry("mods_folder") not in (None, "") and sg.user_settings_get_entry("output_folder") not in (None, ""):
 			sg.user_settings_set_entry("output_zips", values["-OUTPUT ZIPS-"])
