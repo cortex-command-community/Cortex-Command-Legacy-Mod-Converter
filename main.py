c5ecb3c9d8c6dd88fb30d65bdf817ef783479713
MyNameIsTrez <welfje@gmail.com>
2020-12-24 22:27:48 +0100

    Updated the exe so the previous commit is included. Removed the --console flag from the building command in main.py so Windows doesn't think the converter exe is a virus.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Run manually: python main.py
-# Build EXE for CCCP: pyinstaller --noconsole --onefile --icon=Media/cclmc-icon.ico --add-data="Media/github-icon.png;Media" --add-data="Media/discord-icon.png;Media" --add-data="Media/finish.wav;Media" --name="Legacy Mod Converter" main.py
+# Build EXE for CCCP: pyinstaller --onefile --icon=Media/cclmc-icon.ico --add-data="Media/github-icon.png;Media" --add-data="Media/discord-icon.png;Media" --add-data="Media/finish.wav;Media" --name="Legacy Mod Converter" main.py
 
 from Python import gui
 
