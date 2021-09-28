2a2ca48978b51019e90938b68760cea80dd0b1b3
MyNameIsTrez <welfje@gmail.com>
2021-09-28 20:31:40 +0200

    Added the extremely ugly max_length_to_offsets() and max_length_to_offsets_2() functions.

--- Python/ini_parser/ini_rules.py
+++ Python/ini_parser/ini_rules.py
@@ -49,6 +49,8 @@ def apply_rules(parsed_subset):
 				# 	replace_property_and_value(line_data, "MinThrottleRange", "NegativeThrottleMultiplier", min_throttle_range_to_negative_throttle_multiplier)
 				# 	replace_property_and_value(line_data, "MaxThrottleRange", "PositiveThrottleMultiplier", max_throttle_range_to_positive_throttle_multiplier)
 
+				max_length_to_offsets(children)
+
 
 def children_contain_property_shallowly(children, prop):
 	""" This function deliberately doesn't check the section's contents recursively. """
@@ -102,13 +104,81 @@ def replace_property_and_value(line_data, old_property, new_property, value_func
 
 
 def min_throttle_range_to_negative_throttle_multiplier(old_value):
-	new_value = abs(1 - abs(float(token_2["value"])))
+	new_value = abs(1 - abs(float(old_value)))
 	return remove_excess_zeroes(new_value)
 
 
 def max_throttle_range_to_positive_throttle_multiplier(line_data):
-	new_value = abs(1 + abs(float(token_2["value"])))
+	new_value = abs(1 + abs(float(old_value)))
 	return remove_excess_zeroes(new_value)
+
+
+def max_length_to_offsets(children):
+	"""
+	MaxLength = old_value
+	->
+	ContractedOffset = Vector
+		X = old_value / 2
+		Y = 0
+	ExtendedOffset = Vector
+		X = old_value
+		Y = 0
+	"""
+	for index, line_data in enumerate(children):
+		old_value = max_length_to_offsets_2(line_data)
+
+		if old_value != None:
+			# print(index, old_value)
+
+			children.insert(index + 1, [
+				{
+					"type": "property", "value": "ExtendedOffset"
+				},
+				{
+					"type": "value", "value": "Vector"
+				},
+				{
+					"type": "children", "value": [
+						[
+							{ "type": "property", "value": "X" },
+							{ "type": "extra", "value": " "}, {"type": "extra", "value": "="}, {"type": "extra", "value": " "},
+							{ "type": "value", "value": remove_excess_zeroes(old_value) }
+						],
+						[
+							{ "type": "property", "value": "Y" },
+							{ "type": "extra", "value": " "}, {"type": "extra", "value": "="}, {"type": "extra", "value": " "},
+							{ "type": "value", "value": remove_excess_zeroes(0) }
+						]
+					]
+				}
+			])
+
+
+
+def max_length_to_offsets_2(line_data):
+	for token in line_data:
+		if token["type"] == "property":
+			if token["value"] == "MaxLength":
+				token["value"] = "ContractedOffset"
+				for token_2 in line_data:
+					if token_2["type"] == "value":
+						old_value = float(token_2["value"])
+						token_2["value"] = "Vector"
+
+						line_data.append( { "type": "children", "value": [
+							[
+								{ "type": "property", "value": "X" },
+								{ "type": "extra", "value": " "}, {"type": "extra", "value": "="}, {"type": "extra", "value": " "},
+								{ "type": "value", "value": remove_excess_zeroes(old_value / 2) }
+							],
+							[
+								{ "type": "property", "value": "Y" },
+								{ "type": "extra", "value": " "}, {"type": "extra", "value": "="}, {"type": "extra", "value": " "},
+								{ "type": "value", "value": remove_excess_zeroes(0) }
+							]
+						] } )
+
+						return old_value
 
 
 def duplicate_script_path(parsed_subset):
