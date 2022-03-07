9e14ab80ccf767f72c582069daf7d20273321a98
MyNameIsTrez <welfje@gmail.com>
2022-03-07 21:08:53 +0100

    Comment back in tests

--- Python/ini_converting/ini_parser_tests.py
+++ Python/ini_converting/ini_parser_tests.py
@@ -15,62 +15,62 @@ def parser_tests():
 	# 		{ "type": "extra", "content": "\t\t" }, { "type": "property", "content": "Foo" }, { "type": "extra", "content": " " }, { "type": "extra", "content": "=" }, { "type": "extra", "content": " " }, { "type": "value", "content": "Bar" }
 	# 	]
 	# ])
-	# test("comments", [
-	# 	[
-	# 		{ "type": "extra", "content": "\n" },
-	# 		{ "type": "extra", "content": "// foo"}, { "type": "extra", "content": "\n" },
-	# 		{ "type": "extra", "content": "/*a\nb\nc*/" }, { "type": "extra", "content": "\n" },
-	# 	],
-	# ])
-	# test("nested", [
-	# 	[
-	# 		{ "type": "property", "content": "Foo" }, { "type": "extra", "content": " " }, { "type": "extra", "content": "=" }, { "type": "extra", "content": " " }, { "type": "value", "content": "Bar" }, { "type": "extra", "content": "\n" },
-	# 		{ "type": "lines_tokens", "content": [
-	# 			[
-	# 				{ "type": "extra", "content": "\t" }, { "type": "property", "content": "Baz" }, { "type": "extra", "content": " " }, { "type": "extra", "content": "=" }, { "type": "extra", "content": " " }, { "type": "value", "content": "Bee" }, { "type": "extra", "content": "\n" }
-	# 			]
-	# 		]}
-	# 	]
-	# ])
-	# test("multiple", [
-	# 	[
-	# 		{ "type": "property", "content": "Foo" }, { "type": "extra", "content": " " }, { "type": "extra", "content": "=" }, { "type": "extra", "content": " " }, { "type": "value", "content": "Bar" }, { "type": "extra", "content": "\n" },
-	# 		{ "type": "lines_tokens", "content": [
-	# 			[
-	# 				{ "type": "extra", "content": "\t" }, { "type": "property", "content": "Baz" }, { "type": "extra", "content": " " }, { "type": "extra", "content": "=" }, { "type": "extra", "content": " " }, { "type": "value", "content": "Bee" }, { "type": "extra", "content": "\n" }
-	# 			]
-	# 		]}
-	# 	],
-	# 	[
-	# 		{ "type": "property", "content": "A" }, { "type": "extra", "content": " " }, { "type": "extra", "content": "=" }, { "type": "extra", "content": " " }, { "type": "value", "content": "B" }, { "type": "extra", "content": "\n" },
-	# 		{ "type": "lines_tokens", "content": [
-	# 			[
-	# 				{ "type": "extra", "content": "\t" }, { "type": "property", "content": "C" }, { "type": "extra", "content": " " }, { "type": "extra", "content": "=" }, { "type": "extra", "content": " " }, { "type": "value", "content": "D" }, { "type": "extra", "content": "\n" }
-	# 			]
-	# 		]}
-	# 	]
-	# ])
-	# test("complex", [
-	# 	[
-	# 		{ "type": "extra", "content": "\n" },
-	# 		{ "type": "extra", "content": "// foo"}, { "type": "extra", "content": "\n" },
-	# 		{ "type": "extra", "content": "/*a\nb\nc*/" }, { "type": "extra", "content": "\n" },
-	# 		{ "type": "property", "content": "AddEffect" }, { "type": "extra", "content": "  " }, { "type": "extra", "content": "=" }, { "type": "extra", "content": " " }, { "type": "value", "content": "MOPixel" }, { "type": "extra", "content": "//bar" }, { "type": "extra", "content": "\n" },
-	# 		{ "type": "lines_tokens", "content": [
-	# 			[
-	# 				{ "type": "extra", "content": "\t" }, { "type": "property", "content": "PresetName" }, { "type": "extra", "content": " " }, { "type": "extra", "content": "=" }, { "type": "extra", "content": "  " }, { "type": "value", "content": "red_dot_tiny" }, { "type": "extra", "content": "\n" },
-	# 				{ "type": "lines_tokens", "content": [
-	# 					[
-	# 						{ "type": "extra", "content": "\t\t" }, { "type": "property", "content": "Mass" }, { "type": "extra", "content": "  " }, { "type": "extra", "content": "=" }, { "type": "extra", "content": "  " }, { "type": "value", "content": "0.0" }, { "type": "extra", "content": "\n" },
-	# 					],
-	# 					[
-	# 						{ "type": "extra", "content": "\t\t" }, { "type": "property", "content": "Xd" }, { "type": "extra", "content": " " }, { "type": "extra", "content": "=" }, { "type": "extra", "content": " " }, { "type": "value", "content": "42" }
-	# 					]
-	# 				]}
-	# 			]
-	# 		]}
-	# 	]
-	# ])
+	test("comments", [
+		[
+			{ "type": "extra", "content": "\n" },
+			{ "type": "extra", "content": "// foo"}, { "type": "extra", "content": "\n" },
+			{ "type": "extra", "content": "/*a\nb\nc*/" }, { "type": "extra", "content": "\n" },
+		],
+	])
+	test("nested", [
+		[
+			{ "type": "property", "content": "Foo" }, { "type": "extra", "content": " " }, { "type": "extra", "content": "=" }, { "type": "extra", "content": " " }, { "type": "value", "content": "Bar" }, { "type": "extra", "content": "\n" },
+			{ "type": "children", "content": [
+				[
+					{ "type": "extra", "content": "\t" }, { "type": "property", "content": "Baz" }, { "type": "extra", "content": " " }, { "type": "extra", "content": "=" }, { "type": "extra", "content": " " }, { "type": "value", "content": "Bee" }, { "type": "extra", "content": "\n" }
+				]
+			]}
+		]
+	])
+	test("multiple", [
+		[
+			{ "type": "property", "content": "Foo" }, { "type": "extra", "content": " " }, { "type": "extra", "content": "=" }, { "type": "extra", "content": " " }, { "type": "value", "content": "Bar" }, { "type": "extra", "content": "\n" },
+			{ "type": "children", "content": [
+				[
+					{ "type": "extra", "content": "\t" }, { "type": "property", "content": "Baz" }, { "type": "extra", "content": " " }, { "type": "extra", "content": "=" }, { "type": "extra", "content": " " }, { "type": "value", "content": "Bee" }, { "type": "extra", "content": "\n" }
+				]
+			]}
+		],
+		[
+			{ "type": "property", "content": "A" }, { "type": "extra", "content": " " }, { "type": "extra", "content": "=" }, { "type": "extra", "content": " " }, { "type": "value", "content": "B" }, { "type": "extra", "content": "\n" },
+			{ "type": "children", "content": [
+				[
+					{ "type": "extra", "content": "\t" }, { "type": "property", "content": "C" }, { "type": "extra", "content": " " }, { "type": "extra", "content": "=" }, { "type": "extra", "content": " " }, { "type": "value", "content": "D" }, { "type": "extra", "content": "\n" }
+				]
+			]}
+		]
+	])
+	test("complex", [
+		[
+			{ "type": "extra", "content": "\n" },
+			{ "type": "extra", "content": "// foo"}, { "type": "extra", "content": "\n" },
+			{ "type": "extra", "content": "/*a\nb\nc*/" }, { "type": "extra", "content": "\n" },
+			{ "type": "property", "content": "AddEffect" }, { "type": "extra", "content": "  " }, { "type": "extra", "content": "=" }, { "type": "extra", "content": " " }, { "type": "value", "content": "MOPixel" }, { "type": "extra", "content": "//bar" }, { "type": "extra", "content": "\n" },
+			{ "type": "children", "content": [
+				[
+					{ "type": "extra", "content": "\t" }, { "type": "property", "content": "PresetName" }, { "type": "extra", "content": " " }, { "type": "extra", "content": "=" }, { "type": "extra", "content": "  " }, { "type": "value", "content": "red_dot_tiny" }, { "type": "extra", "content": "\n" },
+					{ "type": "children", "content": [
+						[
+							{ "type": "extra", "content": "\t\t" }, { "type": "property", "content": "Mass" }, { "type": "extra", "content": "  " }, { "type": "extra", "content": "=" }, { "type": "extra", "content": "  " }, { "type": "value", "content": "0.0" }, { "type": "extra", "content": "\n" },
+						],
+						[
+							{ "type": "extra", "content": "\t\t" }, { "type": "property", "content": "Xd" }, { "type": "extra", "content": " " }, { "type": "extra", "content": "=" }, { "type": "extra", "content": " " }, { "type": "value", "content": "42" }
+						]
+					]}
+				]
+			]}
+		]
+	])
 
 
 def test(filename, expected):
