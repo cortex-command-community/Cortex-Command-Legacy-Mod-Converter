d9dbb609a32d958e1e4b38e25090fba96cdfba95
MyNameIsTrez <welfje@gmail.com>
2022-03-09 02:52:25 +0100

    Refactor ini_writer.py

--- Python/ini_converting/ini_writer.py
+++ Python/ini_converting/ini_writer.py
@@ -4,24 +4,17 @@ def write_converted_ini_cst(parsed_portion, output_folder_path):
 		if isinstance(dict_or_list, dict): # If dict_or_list contains a dictionary of more filenames.
 			write_converted_ini_cst(dict_or_list, output_folder_path / name)
 		else: # If dict_or_list contains a list of the sections of a file.
-			# pprint.pprint(dict_or_list)
 			with open(str(output_folder_path / name), mode="w") as f:
 				lines = []
 				for section in dict_or_list:
-					get_lines_from_dicts_recursively(section, lines)
-				f.write("\n".join(lines))
+					write_recursively(section, lines)
+				f.write("".join(lines))
 
 
-def get_lines_from_dicts_recursively(line_tokens, lines):
-	# This function loops twice through line_tokens so the lines can be appended in the correct order.
-
-	line = ""
-	for dictionary in line_tokens:
-		if dictionary["type"] != "children":
-			line += dictionary["content"]
-	lines.append(line)
-
-	for dictionary in line_tokens:
-		if dictionary["type"] == "children":
-			for line_tokens in dictionary["content"]:
-				get_lines_from_dicts_recursively(line_tokens, lines)
+def write_recursively(line_tokens, lines):
+	for token in line_tokens:
+		if token["type"] != "children":
+			lines.append(token["content"])
+		elif token["type"] == "children":
+			for line_tokens in token["content"]:
+				write_recursively(line_tokens, lines)
