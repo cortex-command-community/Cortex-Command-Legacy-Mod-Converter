b957b47a62573bd094157ed0e2f55e611d3903ab
MyNameIsTrez <welfje@gmail.com>
2022-03-09 18:47:51 +0100

    Refactor so that is_deeper(), is_same_depth() and is_shallower() all call get_depth()

--- Python/ini_converting/ini_parser.py
+++ Python/ini_converting/ini_parser.py
@@ -15,12 +15,12 @@ def get_parsed_tokens(tokens, parsed=None, token_idx=None, depth=0):
 	while token_idx[0] < len(tokens):
 		token = tokens[token_idx[0]]
 
-		if   state == "newline" and is_deeper(depth, token):
+		if   state == "newline" and is_deeper(depth, token, tokens, token_idx[0] + 1):
 			children = { "type": "children", "content": [] }
 			parsed[-1].append(children)
 			get_parsed_tokens(tokens, children["content"], token_idx, depth + 1)
 			# "state" is deliberately not being changed here.
-		elif state == "newline" and is_same_depth(token, depth):
+		elif state == "newline" and is_same_depth(depth, token, tokens, token_idx[0] + 1):
 			parsed.append([])
 			state = "start"
 		elif state == "newline" and is_shallower(depth, token, tokens, token_idx[0] + 1):
@@ -53,11 +53,11 @@ def get_parsed_tokens(tokens, parsed=None, token_idx=None, depth=0):
 	return parsed
 
 
-def is_deeper(depth, token):
+def is_deeper(depth, token, tokens, next_token_idx):
 	if token["type"] != "TABS":
 		return False
 
-	new_depth = get_depth(token)
+	new_depth = get_depth(token, tokens, next_token_idx)
 
 	if new_depth > depth + 1:
 		line, column = get_token_position(token)
@@ -66,29 +66,32 @@ def is_deeper(depth, token):
 	return new_depth > depth
 
 
-def get_depth(token):
-	return len(token["content"])
-
-
-def is_same_depth(token, depth):
-	return token["type"] == "TABS" and get_depth(token) == depth
-
-
-def is_shallower(depth, token, tokens, next_token_idx):
-	if depth == 0 or token["type"] == "NEWLINES":
-		return False
+def get_depth(token, tokens, next_token_idx):
+	if token["type"] == "NEWLINES":
+		return -1
+	elif token["type"] == "WORD":
+		return 0
 
 	while next_token_idx < len(tokens):
 		next_token = tokens[next_token_idx]
 
 		if next_token["type"] == "WORD":
-			return True
+			return len(token["content"]) # Counts the number of tabs.
 		elif next_token["type"] == "NEWLINES":
-			return False
+			return -1
 
 		next_token_idx += 1
 	
-	return False # Reached when the while-loop read the last character of the file and didn't return.
+	return -1 # Reached when the while-loop read the last character of the file and didn't return.
+
+
+def is_same_depth(depth, token, tokens, next_token_idx):
+	return token["type"] == "TABS" and get_depth(token, tokens, next_token_idx) == depth
+
+
+def is_shallower(depth, token, tokens, next_token_idx):
+	new_depth = get_depth(token, tokens, next_token_idx)
+	return new_depth != -1 and new_depth < depth
 
 
 def get_token_position(token):
