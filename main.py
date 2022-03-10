6b1c299c905831b3589df626a35b35117c668b6a
MyNameIsTrez <welfje@gmail.com>
2022-03-10 05:50:28 +0100

    Remove line that runs tests for the pre4 release

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from Python.gui import gui
 if __name__ == '__main__':
 	try: # TODO: The VS Code Python debugger isn't able to catch exceptions due to this.
 		gui.init_window_theme()
-		tests.run()
+		# tests.run()
 		gui.run_window(gui.init_window())
 	except Exception as e:
 		sg.popup_error("AN EXCEPTION OCCURRED!\n\nYou should make a screenshot of this and either make a GitHub issue for this by clicking the GitHub icon in this program, or you can send it to MyNameIsTrez#1585 on Discord.\n" + "_" * 60, traceback.format_exc(), title="AN EXCEPTION OCCURRED!")
