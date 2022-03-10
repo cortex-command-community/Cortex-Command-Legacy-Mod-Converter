2527dd31adc1b72d2ccc588902dd0d7d694f2493
MyNameIsTrez <welfje@gmail.com>
2022-03-10 05:56:02 +0100

    Add NOT_RELEASE shared_global.py boolean to toggle running tests

--- Python/shared_globals.py
+++ Python/shared_globals.py
@@ -5,6 +5,7 @@ NO_PATH_SET_COLOR = "#b35858"
 CONVERTER_FOLDER_NAME = "_Mod Converter"
 WARNINGS_MOD_NAME_SEPARATOR = "-" * 50
 
+NOT_RELEASE = False # This enables tests when set to True, as the release version shouldn't run tests.
 
 sg = None
 progress_bar = None
--- main.py
+++ main.py
@@ -7,12 +7,14 @@ import PySimpleGUI as sg
 
 from Python import tests
 from Python.gui import gui
+from Python import shared_globals as cfg
 
 
 if __name__ == '__main__':
 	try: # TODO: The VS Code Python debugger isn't able to catch exceptions due to this.
 		gui.init_window_theme()
-		# tests.run()
+		if cfg.NOT_RELEASE:
+			tests.run()
 		gui.run_window(gui.init_window())
 	except Exception as e:
 		sg.popup_error("AN EXCEPTION OCCURRED!\n\nYou should make a screenshot of this and either make a GitHub issue for this by clicking the GitHub icon in this program, or you can send it to MyNameIsTrez#1585 on Discord.\n" + "_" * 60, traceback.format_exc(), title="AN EXCEPTION OCCURRED!")
