a55b2713c1544748240fddd03c43b33f046740d0
MyNameIsTrez <welfje@gmail.com>
2022-03-12 22:50:28 +0100

    Remove annoying warnings that are due to the case checker not taking into account that lines can be commented out

--- Python/case_check/case_check_errors.py
+++ Python/case_check/case_check_errors.py
@@ -2,8 +2,10 @@ from Python import warnings
 
 
 def error_could_not_locate(file_name, line_number, couldnt_be_located):
-	warnings.append_mod_warning(file_name, line_number, "Could not locate", couldnt_be_located)
+	# warnings.append_mod_warning(file_name, line_number, "Could not locate", couldnt_be_located)
+	pass
 
 
 def error_failed_to_find_module(file_name, line_number, module):
-	warnings.append_mod_warning(file_name, line_number, "Failed to find module", module)
+	# warnings.append_mod_warning(file_name, line_number, "Failed to find module", module)
+	pass
