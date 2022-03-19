508fb273fed6c1d4e62bff5fa7719c91b00b45b1
MyNameIsTrez <welfje@gmail.com>
2022-03-19 14:57:04 +0100

    Add ARBITRARILY_HIGH_DEFAULT_GRIP_STRENGTH

--- Python/shared_globals.py
+++ Python/shared_globals.py
@@ -5,6 +5,7 @@ NO_PATH_SET_COLOR = "#b35858"
 VERSION_STRING = f"-v{CONVERTER_VERSION}-{GAME_VERSION}"
 CONVERTER_FOLDER_NAME = "LegacyModConverter" + VERSION_STRING
 WARNINGS_MOD_NAME_SEPARATOR = "-" * 50
+ARBITRARILY_HIGH_DEFAULT_GRIP_STRENGTH = 424242
 
 NOT_RELEASE = True # This enables tests when set to True, as the release version shouldn't run tests.
 
