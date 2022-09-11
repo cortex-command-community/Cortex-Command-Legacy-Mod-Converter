e9f76d6413e932772645696e95b2b6e09a31d259
MyNameIsTrez <welfje@gmail.com>
2022-09-11 17:02:57 +0200

    Remove ASTs

--- Python/run_tests.py
+++ Python/run_tests.py
@@ -1,9 +1,7 @@
 from Python.ini_converting import ini_tokenizer_tests
 from Python.ini_converting import ini_cst_tests
-from Python.ini_converting import ini_ast_tests
 
 
 def run():
 	ini_tokenizer_tests.tokenizer_tests()
 	ini_cst_tests.cst_tests()
-	ini_ast_tests.ast_tests()
