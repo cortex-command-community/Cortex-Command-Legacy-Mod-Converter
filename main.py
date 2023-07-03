af55604b22713dbab717efd29774c0c5520d99fe
MyNameIsTrez <welfje@gmail.com>
2023-07-03 11:36:38 +0200

    Improve error popup layout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import traceback
+import sys
+from traceback import TracebackException
 
 import PySimpleGUI as sg
 
@@ -10,9 +11,17 @@ if __name__ == "__main__":
         gui.init_settings()
         gui.run_window()
     except Exception as e:
+        error = sys.exc_info()[1]
+        stack = sys.exc_info()[2]
+
+        te = TracebackException(type(error), error, stack, limit=None, compact=True)
+        stack_str = "".join(te.stack.format())
+
         sg.popup_error(
-            "AN EXCEPTION OCCURRED!\n\nYou should make a screenshot of this and either make a GitHub issue for this by clicking the GitHub icon in this program, or you can send it to MyNameIsTrez#1585 on Discord.\n"
-            + "_" * 60,
-            traceback.format_exc(),
+            error.args[0] + "_" * 60 + "\n",
+            "If you can't figure out how to fix the above error, you should make a screenshot of this entire window (using Windows key + Shift + S), and either make a GitHub issue for this by clicking the GitHub icon in this program, or you can send this to MyNameIsTrez#1585 on Discord.\n"
+            + "_" * 60
+            + "\n",
+            stack_str,
             title="AN EXCEPTION OCCURRED!",
         )
