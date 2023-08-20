f2c18c88c378d3123f63b37ea6bf55f178f8ecea
MyNameIsTrez <welfje@gmail.com>
2023-08-20 15:00:46 +0200

    Dear ImGui window

---
+++ src/main.zig
@@ -0,0 +1,43 @@
+const std = @import("std");
+
+const zglfw = @import("zglfw");
+const zgpu = @import("zgpu");
+const zgui = @import("zgui");
+
+pub fn main() !void {
+    zglfw.init() catch {
+        std.log.err("Failed to initialize GLFW library.", .{});
+        return;
+    };
+    defer zglfw.terminate();
+
+    const window = zglfw.Window.create(1600, 1000, "Legacy Mod Converter 1.0 for Pre-Release 5.2", null) catch {
+        std.log.err("Failed to create window.", .{});
+        return;
+    };
+    defer window.destroy();
+    window.setSizeLimits(400, 400, -1, -1);
+
+    var gpa_state = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa_state.deinit();
+    const gpa = gpa_state.allocator();
+
+    const gctx = try zgpu.GraphicsContext.create(gpa, window, .{});
+    defer gctx.destroy(gpa);
+
+    zgui.init(gpa);
+    defer zgui.deinit();
+
+    _ = zgui.io.addFontFromFile("content/Roboto-Medium.ttf", 16.0);
+
+    zgui.backend.init(
+        window,
+        gctx.device,
+        @intFromEnum(zgpu.GraphicsContext.swapchain_format),
+    );
+    defer zgui.backend.deinit();
+
+    while (!window.shouldClose()) {
+        zglfw.pollEvents();
+    }
+}
