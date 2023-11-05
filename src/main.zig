cda6e1b4325699bf71e03229719afc4bd50cc49f
MyNameIsTrez <welfje@gmail.com>
2023-11-05 17:21:55 +0100

    Crash progrma if converting goes wrong

--- src/main.zig
+++ src/main.zig
@@ -127,6 +127,8 @@ pub fn main() !void {
                             line,
                             column,
                         });
+
+                        return err;
                     },
                     error.TooManyTabs => {
                         const file_path = diagnostics.file_path orelse "null";
@@ -138,6 +140,8 @@ pub fn main() !void {
                             line,
                             column,
                         });
+
+                        return err;
                     },
                     else => |e| return e,
                 };
