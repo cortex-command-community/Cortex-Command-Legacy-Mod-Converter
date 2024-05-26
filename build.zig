6e7f0ff4f085f30352c355758ce9ffb2240e1345
MyNameIsTrez <welfje@gmail.com>
2024-05-26 12:41:16 +0200

    Clean up build.zig and build.zig.zon

--- build.zig
+++ build.zig
@@ -26,28 +26,19 @@ pub fn build(b: *std.Build) void {
     @import("system_sdk").addLibraryPathsTo(exe);
     @import("zgpu").addLibraryPathsTo(exe);
 
-    const zglfw = b.dependency("zglfw", .{
-        .target = target,
-    });
+    const zglfw = b.dependency("zglfw", .{});
     exe.root_module.addImport("zglfw", zglfw.module("root"));
     exe.linkLibrary(zglfw.artifact("glfw"));
 
-    const zgpu = b.dependency("zgpu", .{
-        .target = target,
-    });
+    const zgpu = b.dependency("zgpu", .{});
     exe.root_module.addImport("zgpu", zgpu.module("root"));
     exe.linkLibrary(zgpu.artifact("zdawn"));
 
     const zgui = b.dependency("zgui", .{
-        .target = target,
         .backend = .glfw_wgpu,
     });
     exe.root_module.addImport("zgui", zgui.module("root"));
     exe.linkLibrary(zgui.artifact("imgui"));
-
-    // TODO: Check if zpool can be removed from libs/ and these lines
-    const zpool = b.dependency("zpool", .{});
-    exe.root_module.addImport("zpool", zpool.module("root"));
 
     const converter = b.addModule("converter", .{
         // TODO: Don't hardcode the path
@@ -83,20 +74,4 @@ pub fn build(b: *std.Build) void {
     // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
-
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_unit_tests = b.addRunArtifact(unit_tests);
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_unit_tests.step);
 }
